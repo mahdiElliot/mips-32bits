cc8e5e6e803d1ce21deaf6e9cca3acb1db0f6118
mahdi <mahdimn2011@yahoo.com>
2019-06-09 18:42:29 +0430

    ...

---
+++ mux5.v
@@ -0,0 +1,11 @@
+    
+module mux5(a, b, c, sel);
+    input sel;
+    input [4:0] a, b;
+    output reg [4:0] c;
+    
+    always @(sel)
+    begin
+        assign c = (sel == 0) ? a : b;
+    end
+endmodule
